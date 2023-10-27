ec9435d7d7ecc1252196b099000f199267929398
alfinkurohidayat <alfinhidayatpro18@gmail.com>
2023-10-27 16:03:40 +0700

    nambah

--- public/js/transaksi.mjs
+++ public/js/transaksi.mjs
@@ -1,8 +1,10 @@
  // Fungsi untuk membuka WhatsApp
  function openWhatsApp() {
+  const msg1 = document.querySelector('.numbers').textContent;
+  const msg2 = document.querySelector('.cardName').textContent;
   // Ganti nomor WA dan pesan sesuai dengan yang Anda inginkan
   var phoneNumber = "6282175577847"; // Ganti dengan nomor WA yang ingin Anda tuju
-  var message = "Halo, saya ingin menghubungi Anda.";
+  var message = `Halo, saya ingin memesan ${msg1} dengan harga ${msg2}.`;
 
   // URL WA dengan nomor dan pesan
   var waURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
