09b23284dc84bb9b0939362a77d4f51c8c93b318
alfinkurohidayat <alfinhidayatpro18@gmail.com>
2024-02-25 22:27:15 +0700

    ubah

--- public/js/index.js
+++ public/js/index.js
@@ -19,3 +19,17 @@ toggle.onclick = function () {
   navigation.classList.toggle("active");
   main.classList.toggle("active");
 };
+
+
+ // Daftar gambar yang akan digunakan
+ const images = ["img/donut.jpg", "img/dp.jpg"];
+ let currentIndex = 0; // Indeks gambar saat ini
+
+ function changeImage() {
+     // Ganti gambar dengan gambar berikutnya dalam daftar
+     currentIndex = (currentIndex + 1) % images.length;
+     document.querySelector(".slide_img").src = images[currentIndex];
+ }
+
+ // Panggil fungsi changeImage setiap beberapa detik (misalnya setiap 5 detik)
+ setInterval(changeImage, 5000); // Ganti gambar setiap 5 detik
